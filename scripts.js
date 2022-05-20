6924057f8f1b117d9b33bfd7e7b9eae30d5f49cd
ValennIglesias <valen.iglesias3@gmail.com>
2022-05-20 13:51:07 -0300

    boton ver mas

--- scripts.js
+++ scripts.js
@@ -3,3 +3,25 @@ var particles = Particles.init({
   color: '#f4d44b',
   connectParticles:true
 });
+
+
+// -------------------------------------
+
+
+let productos = document.getElementById("otrosProductos")
+
+let boton = document.getElementById("VerMas")
+
+
+boton.addEventListener("click", mostrar)
+
+function mostrar(){
+  productos.classList.toggle("mostrar")
+
+  if (productos.classList.contains("mostrar"))  {
+    boton.innerHTML=("VER MENOS")
+    
+  } else {
+    boton.innerHTML=("VER MAS")
+  }
+}
