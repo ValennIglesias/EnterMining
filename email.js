5b4da42144608876d370581b432d4506048632c9
Zoe Lanz <zoe_lanz@hotmail.com>
2022-07-02 20:18:19 -0300

    email js

---
+++ email.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector('.inputBoton');
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    emailjs.init('-e47niS5ubsKQvh4x');
+    event.preventDefault();
+    btn.value = 'Enviando..'
+    emailjs.sendForm('service_elyaujh', 'template_952mjab', this)
+        .then(function () {
+            Toastify({
+                text: "Email enviado, pronto atenderemos su consulta",
+                close: true,
+                duration: 3000,
+                gravity: "top",
+                className: "styleToastify",
+                style: {
+                    background: "black",
+                }
+            }).showToast()
+            btn.value = 'SUBMIT';
+            // alert('Mail status: success');
+        }, function (error) {
+            console.log('Mail status: failed', error);
+        });
+});
